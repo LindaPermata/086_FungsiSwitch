a1a9e5ff3dd3e2c729bad4dddb94c13631a51ac9
LindaPermata <permatalinda531@gmail.com>
2024-03-25 14:53:37 +0700

    membenarkan while

--- calculator.cpp
+++ calculator.cpp
@@ -5,9 +5,9 @@ int bilSatu, bilDua;
 
 void input()
 {
-    cout << "Masukan Bilangan Pertama = ";
+    cout << "masukkan bilangan pertama = ";
     cin >> bilSatu;
-    cout << "Masukan Bilangan Kedua = ";
+    cout << "masukkan bilangan kedua = ";
     cin >> bilDua;
 }
 
@@ -15,14 +15,17 @@ float penjumlahan(float a, float b)
 {
     return a + b;
 }
+
 float pembagian(float a, float b)
 {
     return a / b;
 }
+
 float perkalian(float a, float b)
 {
     return a * b;
 }
+
 float pengurangan(float a, float b)
 {
     return a - b;
@@ -30,51 +33,42 @@ float pengurangan(float a, float b)
 
 int main()
 {
-    int pilihan ; 
-    
-    do
-    {
-        cout <<"===========================" << endl;
+    int pilihan;
+
+    do {
+        cout << "=======================" << endl;
         cout << "MENU" << endl;
-        cout << "1. Penjumlahan" << endl;
-        cout << "2. Pembagian" << endl;
-        cout << "3. Perkalian" << endl;
-        cout << "4. Pengurangan" << endl;
-        cout << "5. Exit " << endl;
-        cout << "Masukan pilihan 1/2/3/4/5 = ";
+        cout << "1. penjumlahan " << endl;
+        cout << "2. pembagian " << endl;
+        cout << "3. perkalian " << endl;
+        cout << "4. pengurangan " << endl;
+        cout << "5. exit " << endl;
+        cout << "masukan pilihan 1/2/3/4/5 = ";
         cin >> pilihan;
 
-        switch(pilihan)
+        switch (pilihan)
         {
-            case 1:
-                input();
-                cout << "Hasil Penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
-                break;
-            case 2:
-                input();
-                cout << "Hasil Pembagian = " << pembagian(bilSatu, bilDua) << endl;
-                break;
-            case 3:
-                input();
-                cout << "Hasil Perkalian = " << perkalian(bilSatu, bilDua) << endl;
-                break;
-            case 4:
-                input();
-                cout << "Hasil Pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
-                break;
-            case 5:
-                break;
-            default;
-                cout << "Pilihan tidak ada " << endl;
-                break;
-
+        case 1 :
+            input();
+            cout << "hasil penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
+            break;
+        case 2 :
+            input();
+            cout << "hasil pembagian = " << pembagian(bilSatu, bilDua) << endl;
+            break; 
+        case 3 :
+            input();
+            cout << "hasil perkalian = " << perkalian(bilSatu, bilDua) << endl;
+            break;
+        case 4 :
+            input();
+            cout << "hasil pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
+            break;
+        case 5 :
+            break;
+        default :
+            cout << "pilihan tidak ada" << endl;
+            break;
         }
-
-    }while(pilihan !=5)
-  
+    }while (pilihan!=5);
 }
-
-
-
-
-
